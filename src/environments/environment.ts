3228fb2dde3d5b1ca69692db981968705d1f17a7
JOMAA <55426203+oussamajomaa@users.noreply.github.com>
2022-11-27 10:36:51 +0100

    Delete environment.ts

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false, 
-    // url: "https://obtic.sorbonne-universite.fr:5001"
-    url: "http://127.0.0.1:5001",
-    siteKey:"6Le-7OYiAAAAANYoAmPdhNWC2WakmiCd_JHZ3bGt"
-  };
-  
